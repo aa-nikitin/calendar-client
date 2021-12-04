9d9d5cbc34207e73f902ccb8899fb51eab5361a6
aa-nikitin <a.nikitin91@mail.ru>
2021-12-05 00:25:48 +0300

    added application formation via paykeeper

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// const site = 'http://81.163.30.95/';
-const site = 'http://localhost:3000/';
+const site = 'http://81.163.30.95/';
+// const site = 'http://localhost:3000/';
 const formatDate = 'DD.MM.YYYY';
 const countOrders = 5;
 
